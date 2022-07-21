d965b9ea7ecc0084bb85924e333574ebf3499765
Evan Niedojadlo <evanniedojadlo@gmail.com>
2022-07-21 14:18:05 -0500

    testing out attaching policies to roles

--- development/iam-roles.tf
+++ development/iam-roles.tf
@@ -1,5 +1,17 @@
 resource "aws_iam_role" "role" {
   name = "test-role"
+  assume_role_policy = aws_iam_group_policy.my_developer_policy
+
+  tags = {
+    managed-by = "terraform"
+  }
+
+}
+
+resource "aws_iam_role" "role2" {
+  name = "test-role2"
+  assume_role_policy = "arn:aws:iam::662799521275:policy/example2"
+
   tags = {
     managed-by = "terraform"
   }
