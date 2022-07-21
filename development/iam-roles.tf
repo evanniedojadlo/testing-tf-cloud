06c68c6a43e2c5588f858f1cfe7591c247119cfa
Evan Niedojadlo <evanniedojadlo@gmail.com>
2022-07-21 15:12:42 -0500

    testing out attaching policies to roles

--- development/iam-roles.tf
+++ development/iam-roles.tf
@@ -1,9 +1,33 @@
+
+
+
 resource "aws_iam_role" "role" {
   name = "test-role"
-  assume_role_policy = aws_iam_group_policy.my_developer_policy
 
-  tags = {
-    managed-by = "terraform"
-  }
-
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
+EOF
+}
+
+# Terraform data resource for managed policies
+data "aws_iam_policy" "ReadOnlyAccess" {
+    arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "attach-managed-admin-policy" {
+  role       =  aws_iam_role.testrole.name
+  policy_arn = "${data.aws_iam_policy.ReadOnlyAccess.arn}"
+}
+
