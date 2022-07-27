41cb837d30337a172f1c02c2cf899963879ab7e9
Evan Niedojadlo <evanniedojadlo@gmail.com>
2022-07-27 17:12:14 -0500

    added test tags to role

--- development/iam-roles.tf
+++ development/iam-roles.tf
@@ -3,6 +3,9 @@
 
 resource "aws_iam_role" "some-test-role" {
   name = "test-role"
+  tags = {
+    managed-by = "terraform"
+  }
 
   assume_role_policy = <<EOF
 {
@@ -30,3 +33,23 @@ resource "aws_iam_role_policy_attachment" "codedeploy_service_role_policy_attach
    role       = "${aws_iam_role.some-test-role.name}"
    policy_arn = "${data.aws_iam_policy.codedeploy_service_policy.arn}"
 }
+
+
+
+
+
+
+
+
+/*
+  tags = {
+    managed-by = "terraform"
+  }
+*/
+
+
+/*
+  tags = {
+    managed-by = "terraform"
+  }
+*/
