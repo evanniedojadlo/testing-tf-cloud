b4309dd9450f3be294b328fd0ee23b13b17b6371
Evan Niedojadlo <evanniedojadlo@gmail.com>
2022-10-27 20:16:35 -0500

    updated vpc name and tags

--- development/utility-vpc.tf
+++ development/utility-vpc.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "utilities-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    environment = "production"
   }
 }
